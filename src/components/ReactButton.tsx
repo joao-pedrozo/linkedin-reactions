39b5e8b1b569ee3242c9a8c0925677ed2e9c8f34
JOAO PEDROZO <jp.pedrozo@hotmail.com>
2023-02-12 11:17:47 -0300

    feat: finish tweet details

--- src/components/ReactButton.tsx
+++ src/components/ReactButton.tsx
@@ -2,45 +2,67 @@ import { useEffect, useState } from "react";
 import Interaction from "./Interaction";
 import { FaRegThumbsUp } from "react-icons/fa";
 
-const interactions = [
+export const interactions: Interaction[] = [
   {
     imageUrl: "https://static.licdn.com/sc/h/f4ly07ldn7194ciimghrumv3l",
     interactionName: "Like",
-    color: "blue",
+    color: "text-blue-600",
+    background: "bg-blue-200",
   },
   {
     imageUrl: "https://static.licdn.com/sc/h/3c4dl0u9dy2zjlon6tf5jxlqo",
     interactionName: "Celebrate",
-    color: "green",
+    color: "text-green-600",
+    background: "bg-green-400",
   },
   {
     imageUrl: "https://static.licdn.com/sc/h/9whrgl1hq2kfxjqr9gqwoqrdi",
     interactionName: "Support",
-    color: "purple",
+    color: "text-purple-600",
+    background: "bg-purple-300",
   },
   {
     imageUrl: "https://static.licdn.com/sc/h/ktcgulanbxpl0foz1uckibdl",
     interactionName: "Funny",
-    color: "cyan",
+    color: "text-cyan-600",
+    background: "bg-cyan-300",
   },
   {
     imageUrl: "https://static.licdn.com/sc/h/asmf650x603bcwgefb4heo6bm",
     interactionName: "Love",
-    color: "red",
+    color: "text-red-600",
+    background: "bg-red-300",
   },
   {
     imageUrl: "https://static.licdn.com/sc/h/39axkb4qe8q95ieljrhqhkxvl",
     interactionName: "Insightful",
-    color: "yellow",
+    color: "text-yellow-600",
+    background: "bg-yellow-300",
   },
 ];
 
+interface Interaction {
+  imageUrl: string;
+  interactionName: string;
+  color: string;
+  background: string;
+}
+
 const ReactButton = () => {
   const [isHovered, setIsHovered] = useState(false);
-  const [interaction, setInteraction] = useState<string | null>(null);
+  const [interaction, setInteraction] = useState<Interaction | null>(null);
 
   const setInteractionHandler = (interaction: string) => {
-    setInteraction(interaction);
+    const findedInteraction = interactions.find(
+      (interactionItem) => interactionItem.interactionName === interaction
+    );
+
+    setInteraction({
+      imageUrl: findedInteraction!.imageUrl,
+      interactionName: findedInteraction!.interactionName,
+      color: findedInteraction!.color,
+      background: findedInteraction!.background,
+    });
   };
 
   useEffect(() => {
@@ -51,32 +73,20 @@ const ReactButton = () => {
 
   return (
     <button
-      className="relative flex justify-center flex-1 py-2 font-semibold h-14 w-14"
+      className="relative flex justify-center flex-1 font-semibold py-4 w-14 hover:bg-slate-100 rounded-xl"
       onMouseOver={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       {interaction ? (
-        <div className="flex gap-2 justify-center align-middle">
+        <div
+          className={`flex flex-1 gap-2 justify-center items-center w-full max-h-full ${interaction.color}`}
+        >
           <img
-            src={
-              interactions.find(
-                (interactionItem) =>
-                  interactionItem.interactionName === interaction
-              )?.imageUrl
-            }
+            src={interaction.imageUrl}
             alt="React Logo"
-            className="w-7 h-7 transition-all duration-300 ease-in-out group-hover:translate-y-[-15px] group-hover:scale-125 mt-[-5px]"
+            className="w-7 h-7 transition-all duration-300 ease-in-out mt-[-5px] group-hover:translate-y-[-15px] group-hover:scale-125"
           />
-          <span
-            className={`text-${
-              interactions.find(
-                (interactionItem) =>
-                  interactionItem.interactionName === interaction
-              )?.color
-            }-500`}
-          >
-            {interaction}
-          </span>
+          <span>{interaction.interactionName}</span>
         </div>
       ) : (
         <div
--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,6 +1,6 @@
-import ReactButton from "./ReactButton";
+import ReactButton, { interactions } from "./ReactButton";
 import { BiComment } from "react-icons/bi";
-import { AiOutlineRetweet } from "react-icons/ai";
+import { AiOutlineRetweet, AiOutlinePlus } from "react-icons/ai";
 import { GrSend } from "react-icons/gr";
 
 const Tweet = () => {
@@ -8,15 +8,27 @@ const Tweet = () => {
     <div className="bg-gray-100 w-full h-full flex justify-center">
       <div className="max-w-xl w-full items-center justify-center flex">
         <div className="bg-white p-4">
-          <div className="flex gap-4">
+          {/* <img
+            src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+            className="w-10 h-10 mb-8"
+          /> */}
+          <div className="flex gap-4 mb-4">
             <img
               src="https://conteudo.imguol.com.br/c/noticias/8c/2022/11/22/02mai2022---o-bilionario-elon-musk-no-met-gala-em-nova-york-1669125518979_v2_3x4.jpg"
-              className="rounded-full w-16 h-16 object-cover"
+              className="rounded-full w-16 h-16 object-cover flex flex-shrink-0"
             />
-            <div className="flex flex-col">
-              <b>Elon Musk</b>
-              <span>SpaceX CEO and Tesla Co-Founder</span>
-              <span>1h</span>
+            <div className="flex justify-between w-full items-center">
+              <div className="flex flex-col">
+                <b className="font-semibold">Elon Musk</b>
+                <span className="text-gray-400 leading-4">
+                  SpaceX CEO and Tesla Co-Founder
+                </span>
+                <span className="text-gray-400">1h</span>
+              </div>
+              <div className="flex gap-1 text-blue-600 cursor-pointer hover:bg-blue-200 p-1 rounded-md transition-all duration-200 ease-in-out">
+                <AiOutlinePlus size={24} />
+                <span className="text-lg leading-6">Follow</span>
+              </div>
             </div>
           </div>
           <span>
@@ -41,21 +53,79 @@ const Tweet = () => {
             <b className="font-semibold">#BrazilianTechWonder</b>{" "}
             <b className="font-semibold">#SoftwareSuperstar</b>
           </span>
+          <div className="flex mt-2 mb-1 ml-2">
+            {interactions.slice(0, 3).map((interaction) => (
+              <div
+                className={`bg-red-200 rounded-full w-5 h-5 flex justify-center items-center p-[3px] ml-[-7px] ${interaction.background}`}
+              >
+                <img src={interaction.imageUrl} className="h-5 w-5" />
+              </div>
+            ))}
+            <span className="ml-1 text-gray-400 leading-5">
+              Kent C. Dodds and 10.923 others
+            </span>
+          </div>
           <hr className="w-full my-2 border-1 inline-block" />
-          <div className="flex">
+          <div className="flex mb-4">
             <ReactButton />
-            <button className="flex flex-1 justify-center gap-2 py-2 font-semibold w-14">
+            <button className="flex flex-1 justify-center items-center hover:bg-slate-100 rounded-xl gap-2 font-semibold w-14 py-4 opacity-30">
               <BiComment size={22} />
               <span>Comment</span>
             </button>
-            <button className="flex flex-1 justify-center gap-2 py-2 font-semibold w-14">
+            <button className="flex flex-1 justify-center items-center hover:bg-slate-100 rounded-xl gap-2 font-semibold w-14 py-4 opacity-30">
               <AiOutlineRetweet size={22} />
               <span>Repost</span>
             </button>
-            <button className="flex flex-1 justify-center gap-2 py-2 font-semibold w-14">
+            <button className="flex flex-1 justify-center items-center hover:bg-slate-100 rounded-xl gap-2 font-semibold w-14 py-4 opacity-30">
               <GrSend size={22} />
               <span>Send</span>
             </button>
+          </div>
+          <div className="flex gap-2">
+            <img
+              src="/joao-pedrozo.png"
+              className="w-12 h-12 rounded-full object-cover flex flex-shrink-0"
+            />
+            <div>
+              <div className="bg-gray-100 p-2 w-full">
+                <div className="flex justify-between">
+                  <div className="mb-3">
+                    <b className="font-semibold">João Pedrozo</b>
+                    <span className="block text-gray-400 leading-4 text-sm">
+                      Software Engineer
+                    </span>
+                  </div>
+                  <span className="text-slate-400">25m</span>
+                </div>
+                <span>
+                  Yo Elon! Thanks bro, I'm seriously hyped about your shoutout.
+                  I've got a passion for all things tech and I love nothing more
+                  than working with cutting-edge solutions. You know me, always
+                  pushing the boundaries!
+                </span>
+              </div>
+              <div className="mt-2 flex items-center">
+                <b className="font-medium text-slate-400 ml-2 cursor-pointer">
+                  Like
+                </b>
+                <span className="leading-3 mt-[-5px] text-slate-400 mx-2">
+                  .
+                </span>
+                <div className="flex items-center">
+                  {interactions.slice(0, 3).map((interaction) => (
+                    <div
+                      className={`bg-red-200 rounded-full w-[19px] h-[19px] flex justify-center items-center p-[3px] ml-[-7px] ${interaction.background} first:ml-0`}
+                    >
+                      <img
+                        src={interaction.imageUrl}
+                        className="h-[19px] w-[19px]"
+                      />
+                    </div>
+                  ))}
+                  <span className="text-slate-400 text-md ml-1">3.369</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
